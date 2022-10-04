f73b1740b577e14a537c64f46e0a78053272f0ba
Evelin Velikov <43967421+Silener@users.noreply.github.com>
2022-10-04 14:14:04 +0300

    Add files via upload

---
+++ S3Connector/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace S3Connector
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.accessKeyIDTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.secretAccessKeyTextBox = new System.Windows.Forms.TextBox();
+            this.regionComboBox = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.localDirectoryTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.bucketLinkTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // accessKeyIDTextBox
+            // 
+            this.accessKeyIDTextBox.Location = new System.Drawing.Point(12, 27);
+            this.accessKeyIDTextBox.Name = "accessKeyIDTextBox";
+            this.accessKeyIDTextBox.Size = new System.Drawing.Size(292, 23);
+            this.accessKeyIDTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Access Key ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Secret Access Key";
+            // 
+            // secretAccessKeyTextBox
+            // 
+            this.secretAccessKeyTextBox.Location = new System.Drawing.Point(12, 88);
+            this.secretAccessKeyTextBox.Name = "secretAccessKeyTextBox";
+            this.secretAccessKeyTextBox.Size = new System.Drawing.Size(292, 23);
+            this.secretAccessKeyTextBox.TabIndex = 3;
+            // 
+            // regionComboBox
+            // 
+            this.regionComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.regionComboBox.FormattingEnabled = true;
+            this.regionComboBox.Location = new System.Drawing.Point(12, 148);
+            this.regionComboBox.Name = "regionComboBox";
+            this.regionComboBox.Size = new System.Drawing.Size(292, 23);
+            this.regionComboBox.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Region";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 190);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Local Directory";
+            // 
+            // localDirectoryTextBox
+            // 
+            this.localDirectoryTextBox.Location = new System.Drawing.Point(12, 208);
+            this.localDirectoryTextBox.Name = "localDirectoryTextBox";
+            this.localDirectoryTextBox.Size = new System.Drawing.Size(292, 23);
+            this.localDirectoryTextBox.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 251);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Bucket Link";
+            // 
+            // bucketLinkTextBox
+            // 
+            this.bucketLinkTextBox.Location = new System.Drawing.Point(12, 269);
+            this.bucketLinkTextBox.Name = "bucketLinkTextBox";
+            this.bucketLinkTextBox.Size = new System.Drawing.Size(292, 23);
+            this.bucketLinkTextBox.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(199, 314);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Download";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 350);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.bucketLinkTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.localDirectoryTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.regionComboBox);
+            this.Controls.Add(this.secretAccessKeyTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.accessKeyIDTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox accessKeyIDTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox secretAccessKeyTextBox;
+        private System.Windows.Forms.ComboBox regionComboBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox localDirectoryTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox bucketLinkTextBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ S3Connector/Form1.cs
@@ -0,0 +1,148 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace S3Connector
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            regionComboBox.Items.Add("EUWest2");
+            regionComboBox.Items.Add("EUSouth1");
+            regionComboBox.Items.Add("EUWest3");
+            regionComboBox.Items.Add("USEast2");
+            regionComboBox.Items.Add("EUWest1");
+            regionComboBox.Items.Add("EUNorth1");
+            regionComboBox.Items.Add("USWest1");
+            regionComboBox.Items.Add("USWest2");
+            regionComboBox.Items.Add("USEast1");
+        }
+
+
+        public void OnOKClick()
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string errorMessages = "";
+
+            if( accessKeyIDTextBox.Text.Length <= 0 )
+            {
+                errorMessages += "Please fill Access Key ID.\n";
+            }// if
+
+            if (secretAccessKeyTextBox.Text.Length <= 0)
+            {
+                errorMessages += "Please fill Secret Access Key.\n";
+            }// if
+
+            if (regionComboBox.SelectedIndex <= -1)
+            {
+                errorMessages += "Please fill Region.\n";
+            }// if
+
+            if (localDirectoryTextBox.Text.Length <= 0)
+            {
+                errorMessages += "Please fill Local Directory.\n";
+            }// if
+
+            if (bucketLinkTextBox.Text.Length <= 0)
+            {
+                errorMessages += "Please fill Bucket Link.\n";
+            }// if
+
+            if(errorMessages.Length > 0)
+            {
+                MessageBox.Show(errorMessages);
+                return;
+            }
+
+            IAmazonS3 client = new AmazonS3Client(accessKeyIDTextBox.Text, secretAccessKeyTextBox.Text, regionComboBox.Text);
+
+        }
+    }
+}
+
+namespace Amazon.DocSamples.S3
+{
+    class GetObjectTest
+    {
+        private string bucketName;
+
+
+
+        private string keyName;
+
+        GetObjectTest( string accessKeyID, string secretAccessKey, string selectedRegion, string bucketLink, string localDirectory )
+        {
+
+        }
+
+        private const string bucketName = "bukkket";
+        private const string keyName = "ТЕСТ";
+        // Specify your bucket region (an example region is shown).
+        private static readonly RegionEndpoint bucketRegion = RegionEndpoint.EUWest2;
+        private static IAmazonS3 client;
+       //public static void Main()
+       //{
+       //    client = new AmazonS3Client(bucketRegion);
+       //    ReadObjectDataAsync().Wait();
+       //}
+
+        static async Task ReadObjectDataAsync()
+        {
+            try
+            {
+                GetObjectRequest request = new GetObjectRequest
+                {
+                    BucketName = bucketName,
+                    Key = keyName
+
+                };
+
+                GetObjectResponse response = await client.GetObjectAsync(request);
+
+                int numBytesToRead = (int)response.ContentLength;
+                if (numBytesToRead > 0)
+                {
+                    int numBytesRead = 0;
+                    byte[] buffer = new byte[32768];
+                    using (FileStream fstream = new FileStream("D:\\3.txt", FileMode.Create))
+                    {
+                        using (Stream responseStream = response.ResponseStream)
+                        {
+                            do
+                            {
+                                numBytesRead = responseStream.Read(buffer, 0, buffer.Length);
+                                fstream.Write(buffer, 0, numBytesRead);
+                            }
+                            while (numBytesRead > 0);
+                        }
+                    }
+                }
+            }
+            catch (AmazonS3Exception e)
+            {
+                // If bucket or object does not exist
+                Console.WriteLine("Error encountered ***. Message:'{0}' when reading object", e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unknown encountered on server. Message:'{0}' when reading object", e.Message);
+            }
+        }
+    }
+}
